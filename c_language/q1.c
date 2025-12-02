8c937a5e72e590a43b536255c6da430ff4f5a040
Sushant-Ravi14 <225856552+Sushant-Ravi14@users.noreply.github.com>
2025-12-02 14:26:34 +0530

    Implement sum calculation for second row of matrix
    Calculate the sum of the second row in a 3x3 matrix.

--- c_language/q1.c
+++ c_language/q1.c
@@ -1,1 +1,17 @@
-test
+#include <stdio.h>
+int main(){
+    int mat[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int sum = 0;
+    
+    for(int r=0; r<3; r++){
+        if(r==1){
+            for(int c=0; c<3; c++){
+                sum += mat[1][c];
+            }
+        }
+    }
+    
+    printf("Sum :%d",sum);
+    return 0;
+}
+
